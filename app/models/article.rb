fabf1d6da7da28a007958f5b6ba3c01342b7fce1
Ricky9011 <rpchubukova@gmail.com>
2022-11-11 09:44:11 +0300

    Add a Static Pages controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     validates :tittle, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
-
+    belongs_to :blog
     
     
 end
--- app/models/hatchery.rb
+++ app/models/hatchery.rb
@@ -1,2 +1,4 @@
 class Hatchery < ApplicationRecord
+    has_many :blogs
+    has_one :breeder 
 end
