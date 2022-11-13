23137307f89043406dd77a465ee8316dee5f4e0a
Ricky9011 <rpchubukova@gmail.com>
2022-11-13 17:18:05 +0300

    Add a Static Pages controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+let odds = [0,2,4,6,8].map(v => v + 1);
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_223635) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_065004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_223635) do
     t.string "name"
     t.text "description"
     t.text "history"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "breeder_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
